012d4d7dfdcb3a36646d0ad5186d9f490bca9e8c
Amir Hossein Baradaran Toutounchi <amirhossein.baradarantutunchi@adevinta.com>
2023-06-03 14:28:55 +0200

    converts json correctly to Location model on 200 response

--- ABNLocations/ABNLocationsTests/LocationAPI/LocationMapperTests.swift
+++ ABNLocations/ABNLocationsTests/LocationAPI/LocationMapperTests.swift
@@ -21,7 +21,7 @@ final class LocationMapperTests: XCTestCase {
         )
     }
     
-    func test_map_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() throws {
+    func test_map_deliversNoLocationsOn200HTTPResponseWithEmptyJSONList() throws {
         let emptyListJSON = makeLocationsJSON([])
         
         let result = try LocationMapper.map(emptyListJSON, from: HTTPURLResponse(statusCode: 200))
@@ -29,6 +29,17 @@ final class LocationMapperTests: XCTestCase {
         XCTAssertEqual(result, [])
     }
     
+    func test_map_deliversLocationsOn200HTTPResponseWithJSONItems() throws {
+        let item1 = makeItem(name: nil, lat: 23.432423, long: 14.7678)
+        
+        let item2 = makeItem(name: "barcelona", lat: 32.432423, long: 41.7678)
+        
+        let json = makeLocationsJSON([item1.json, item2.json])
+        
+        let result = try LocationMapper.map(json, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(result, [item1.model, item2.model])
+    }
 }
 
 // MARK: - Test Helpers
@@ -37,5 +48,24 @@ extension LocationMapperTests {
     func makeLocationsJSON(_ items: [[String: Any]]) -> Data {
         let json = ["locations": items]
         return try! JSONSerialization.data(withJSONObject: json)
+    }
+    
+    private func makeItem(
+        name: String? = nil,
+        lat: Double,
+        long: Double
+    ) -> (
+        model: Location,
+        json: [String: Any]
+    ) {
+        let location = Location(name: name, latitude: lat, longitude: long)
+        
+        let json = [
+            "name": name as Any,
+            "lat": lat,
+            "long": long
+        ].compactMapValues { $0 }
+        
+        return (location, json)
     }
 }
